38a9497688cc6d2d49215702955c17065e411ba4
KU-Baha <kulbarakovich@gmail.com>
2023-04-28 19:41:12 +0600

    Fix bug with limit

--- apps/wallet/signals.py
+++ apps/wallet/signals.py
@@ -5,10 +5,10 @@ from django.dispatch import receiver
 from .models import TransactionImage
 
 
-@receiver(pre_save, sender=TransactionImage)
-def limit_transaction_images(sender, instance, **kwargs):
-    if instance.pk:
-        return
-
-    if instance.transaction.images.count() >= 2:
-        raise ValidationError("Transaction can only have 2 images.")
+# @receiver(pre_save, sender=TransactionImage)
+# def limit_transaction_images(sender, instance, **kwargs):
+#     if instance.pk:
+#         return
+#
+#     if instance.transaction.images.count() >= 5:
+#         raise ValidationError("Transaction can only have 5 images.")
